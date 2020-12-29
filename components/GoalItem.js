42c0fe55e0be806477d82e4d730c94a8bd6d4131
kimjinsoo <roorooro9933@gmail.com>
2020-12-29 17:02:14 +0900

    delete items

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -3,10 +3,10 @@ import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
 const GoalItem = (props) => {
   return (
-    // 이렇게 Highlight를 쓰고 underlayColor를 하면
-    // Touch 했을 때 , background 색깔을 바꿔준다는 것이다.
-    // 뭐 이것 말고도 엄청 다양하겠지만, 이정도 있다는 것 정도만 알아두자.
-    <TouchableOpacity activeOpacity={0.8} onPress={props.onDelete}>
+    <TouchableOpacity
+      activeOpacity={0.8}
+      onPress={() => props.onDelete(props.id)}
+    >
       <View style={styles.listItem}>
         <Text>{props.title}</Text>
       </View>
