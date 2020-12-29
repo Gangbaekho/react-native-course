14b0918115df2db3c9639a5ab2936738810e79f9
kimjinsoo <roorooro9933@gmail.com>
2020-12-29 11:59:30 +0900

    how to use stylesheet objects

--- App.js
+++ App.js
@@ -4,23 +4,9 @@ import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
   return (
-    <View style={{ padding: 30 }}>
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-          alignItems: "center",
-        }}
-      >
-        <TextInput
-          placeholder="Course Goals"
-          style={{
-            borderColor: "black",
-            borderWidth: 1,
-            padding: 10,
-            width: "80%",
-          }}
-        />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Course Goals" style={styles.input} />
         <Button title="ADD" />
       </View>
       <View></View>
@@ -28,4 +14,23 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({});
+// StyleSheet.create를 안쓰고 그냥 {}로 줘도
+// 먹히긴 하는데, 최적화나 여러가지 관련해서
+// 더 추가해주는 기능이 있으니까 그냥 이렇게 쓰도록 해라.
+
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  input: {
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10,
+    width: "80%",
+  },
+});
